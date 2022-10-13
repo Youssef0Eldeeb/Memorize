399646926ce3d22e31ff564ccc00e2df3e9d62a7
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2022-10-14 00:47:24 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Youssef Eldeeb on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
